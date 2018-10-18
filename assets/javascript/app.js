e7e0a1e248cf140c1a69e4396aab82c34937258b
Charlie Paull <charliepaull211@gmail.com>
2018-10-17 18:38:47 -0700

    starting to set clock for countdown

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,28 +13,56 @@
             Take Quiz Again
 */
 
-// Goal: Declare necessary variables.
-// Declare var secondRemain & set to 0.
-var secondsRemain = 60;
-// Declare var clockRunning & set to false.
-var clockRunning = false;
 
-// Create a function declaration & name it startCount
+// Select document and use the .ready() method to run JS code after a page loads
 $(document).ready(function(){
-    $(".header-container").hide("medium");
+    // Goal: Declare necessary variables.
+    // Declare var clockRunning & set to false.
+    var clockRunning = false;
+    // Declare an intervalId variable
+    var intervalId;
+
+    // Goal: Everything except Start Button hidden. User clicks on Start Button to begin game.
+    /* Select the class .header-container & use a method to hide it 
+    (use ("fast") as parameter)*/
+    $(".header-container").hide("fast");
+    /*Selected button element & use click method & create 
+    a declare a callback function named clickStart*/
         $("button").click(function clickStart() {
-            $("#button").hide();
+            $("#button").hide(0);
             $(".header-container").show();
     });
-});
 
-// // // Goal: Everything except Start Button hidden. User clicks on Start Button to begin game.
-// // // Select the header div & create a callback function that selects class ".header-container" & hides the content.
-// // // Select the button element & create callback function to click the "Start" button.
-// //     $("button").click(function clickStart() {
-// //     // select ID associated with the button & hide button.
-// //         $("#button").hide();
-// //         $(".header-container").show();
-
+    // Goal: Begin time countdown
+    // Declare a countdown variable & make it an object
+    var stopwatch = {
+        // set key of time equal to the value of 60 seconds
+        time: 60,
+    // Create an start function
+    start: function() {
+        /*Select ID #seconds-remain & replace it's text dynamically
+        add: "Seconds Remaining: " + 60   into tag. */
+        $("#seconds-remain").text("Seconds Remaining: " + 60);
+        // Declare an IF statement using (!clockRunning)
+        if (!clockRunning) {
+        // mutate setInterval to var interval.Id() & start count, pace by 1 second
+            intervalId = setInterval(stopwatch.count, 1000);
+            // set clock to start running
+            clockRunning = true;
+            // decrease stopwatch.time total by 1
+            stopwatch.time--;
+       } 
+    }
+    }
 //     function startCount(){
 //         // Use setInterval to start Count
+
+// Create a function declaration & name it startCount
+
+
+
+
+
+
+
+});
